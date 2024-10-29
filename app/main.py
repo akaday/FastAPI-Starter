2f54bc339bd1ad2b841aa4b277b8edee0e1a35b5
aka <akadev8@outlook.com>
2024-10-29 01:11:00 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI Starter by Akaday!"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
