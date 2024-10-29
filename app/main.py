215ffad73459fec6e9b0595b95e7f7a3c16d1280
aka <akadev8@outlook.com>
2024-10-29 01:25:55 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from .database import database, engine, metadata
+from .models import users
 
+metadata.create_all(engine)
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI Starter by Akaday!"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
-# app/main.py (add CRUD routes)
+
 @app.post("/users/")
 async def create_user(name: str, email: str):
     query = users.insert().values(name=name, email=email)
